fda55e4db6da8d393bc2efed0c73f0863229f56b
Nikit Soni <nikitsoni17@gmail.com>
2024-06-28 21:09:25 +0530

    Sudoku Solver

--- main.py
+++ main.py
@@ -1,81 +1,83 @@
 from tabulate import tabulate
-from collections import defaultdict
 import time
+import os
+from typing import List, Set
 
 class SudokuSolver:
     
-    def __init__(self):
-        
-        self.row = defaultdict(set)
-        self.column = defaultdict(set)
-        self.square = defaultdict(set)
-        self.result = []
+    def backtracking(self, board: List[List[str]], row: int, col: int, rows_values: List[Set[str]], cols_values:List[Set[str]], row_col_values: List[List[Set[str]]]) -> True:
     
-    def verify(self, value, row, col):
-        
-        if value in self.row[row] or value in self.column[col] or value in self.square[(row // 3, col // 3)]:
-            return False
+        clear_screen()
+        print_sudoku(board)
+        time.sleep(0.001)
 
-        return True
-
-    def backtrack_solver(self, idx, board):
-        
-        if idx == 81:
-            temp_board = board.copy()
-            self.result = temp_board
+        if row == 9:
             return True
         
-        row = idx // 9
-        col = idx % 9
-
-        if board[row][col] != ".":
-            return self.backtrack_solver(idx + 1, board)
-        else:
-            for val in range(1, 10):
-                if self.verify(val, row, col):
-                    
-                    self.row[row].add(val)
-                    self.column[col].add(val)
-                    self.square[(row//3, col//3)].add(val)
-
-                    board[row][col] = str(val)
-                    # print_sudoku(board)
-                    # time.sleep(1)
-
-                    if self.backtrack_solver(idx + 1, board):
-                        return True
-
-                    self.row[row].remove(val)
-                    self.column[col].remove(val)
-                    self.square[(row//3, col//3)].remove(val)
-            
-            return False 
-
-
-    def solve_board(self, board):
+        if col == 9:
+            return self.backtracking(board, row + 1, 0, rows_values, cols_values, row_col_values)
         
-        # Add given numbers in the respective positional hashmap
+        if board[row][col] != ".":
+            return self.backtracking(board, row, col + 1, rows_values, cols_values, row_col_values)
+        
+        for digit in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            
+            if not digit in rows_values[row] and not digit in cols_values[col] and not digit in row_col_values[row//3][col//3]:
+                
+                board[row][col] = digit
+                rows_values[row].add(digit)
+                cols_values[col].add(digit)
+                row_col_values[row//3][col//3].add(digit)
+                
+                if self.backtracking(board, row, col+1, rows_values, cols_values, row_col_values):
+                    return True
+                
+                board[row][col] = "."
+                rows_values[row].remove(digit)
+                cols_values[col].remove(digit)
+                row_col_values[row//3][col//3].remove(digit)
+        
+        return False
+
+    def solveSudoku(self, board: List[List[str]]) -> None:
+       
+        rows_values: List[Set[str]] = [set() for _ in range(9)]
+        cols_values:List[Set[str]] = [set() for _ in range(9)] 
+        row_col_values: List[List[Set[str]]] = [
+            [set() for _ in range(9)] 
+            for _ in range(9)
+        ]
+
         for row in range(9):
             for col in range(9):
-                num = board[row][col]
-                if num != ".":
-                    self.row[row].add(num)
-                    self.column[col].add(num)
-                    self.square[(row // 3, col // 3)].add(num)
+                digit = board[row][col] 
+                if digit != ".":
+                    rows_values[row].add(digit)
+                    cols_values[col].add(digit)
+                    row_col_values[row//3][col//3].add(digit)
         
-        # Initiate Backtracking for the solution.
-        self.backtrack_solver(0, board)
-        return self.result
+        for row in range(9):
+            for col in range(9):
+                digit = board[row][col] 
+                if digit == ".":
+                    self.backtracking(board, row, col, rows_values, cols_values, row_col_values)
+        
+        return board
+
+
 
 def print_sudoku(board):
     # Replace dots and zeros with underscore for a cleaner look
     formatted_board = [['_' if num == 0 or num == '.' else num for num in row] for row in board]
 
-    # Prepare the table with tabulaßte
-    table = tabulate(formatted_board, tablefmt="fancy-grid", numalign="center", stralign="center")
+    # Prepare the table with tabulate
+    table = tabulate(formatted_board, tablefmt="mixed_grid", numalign="center", stralign="center")
 
     print(table)
 
+def clear_screen():
+    # Clear the screen
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
 
@@ -91,11 +93,8 @@ def main():
         [".",".",".",".","8",".",".","7","9"]
     ]
     
-    print("Unsolved Board: ")
-    print_sudoku(board)
-
     sudoku_solver_instance = SudokuSolver()
-    solved_board = sudoku_solver_instance.solve_board(board)
+    solved_board = sudoku_solver_instance.solveSudoku(board)
     
     print("Solved Board: ")
     print_sudoku(solved_board)
